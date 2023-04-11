cae29ef422c727580fd2064ffa094a18039ce02c
Khushahal Sharma <sharmapckhushal007@gmail.com>
2023-04-11 12:18:01 +0530

    bodyparserUn

--- Backend/index.js
+++ Backend/index.js
@@ -4,15 +4,11 @@ dotenv.config()
 const db=require('./db')
 const calendarModel=require('./Model/calenderModel')
 const {calenderController}=require('./Routes/calenderRoute')
-const bodyParser = require("body-parser");
-
-// const cors=require("cors")
 
 const app = express()
 
-// app.use(express.json());
-app.use(bodyParser.json());
-// app.use(cors())
+app.use(express.json());
+
 app.get('/', (req, res) => {
    res.send("Server is running Succcessfuly")
 })
